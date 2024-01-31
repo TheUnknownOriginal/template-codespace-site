d693fadd5fb0bc62132d44b7d8869ea4898d0779
TheUnknownOriginal <86056658+TheUnknownOriginal@users.noreply.github.com>
2024-01-31 08:17:21 +0000

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Leong Yung Zheng",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "yungzheng90@gmail.com",
+  gitHub: "https://github.com/TheUnknownOriginal",
+  instagram: "https://www.instagram.com/yungzheng1412/",
+  linkedIn: "https://www.linkedin.com/in/yung-zheng-leong-30870b209/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "https://www.youtube.com/channel/UCgtJ876gIQysmjchQm8rGwA",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,26 +16,24 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/stars-liine.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "timelapse of star";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Software Engineering student studying at University Malaya. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
+// eslint-disable-next-line prettier/prettier
 const skillsList = [
   "Web design",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
   "Graphic design",
 ];
 
